9f73bebb08ee7c04a60236e22845c322fe558647
jws <350331045@qq.com>
2022-12-05 15:51:51 +0800

    update

--- AstroNvim/user/configs/vscode-js.lua
+++ AstroNvim/user/configs/vscode-js.lua
@@ -1,57 +1,96 @@
--- npm install --legacy-peer-deps && npm run compile
-require("dap-vscode-js").setup {
-  node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-  debugger_path = "~/.local/share/nvim/mason/packages/js-debug-adapter", -- Path to vscode-js-debug installation.
-  -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-  adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
-}
+-- setup adapters
+require("dap-vscode-js").setup({
+        debugger_path = vim.fn.stdpath("data") .. "/mason/packages/js-debug-adapter",
+        debugger_cmd = { "js-debug-adapter" },
+        adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" },
+})
 
-for _, language in ipairs { "typescript", "javascript" } do
-  require("dap").configurations[language] = {
-    {
-      type = "pwa-node",
-      request = "launch",
-      name = "Launch file",
-      program = "${file}",
-      cwd = "${workspaceFolder}",
-    },
-    {
-      type = "pwa-node",
-      request = "attach",
-      name = "Attach",
-      processId = require("dap.utils").pick_process,
-      cwd = "${workspaceFolder}",
-    },
+local dap = require("dap")
 
-    {
-      name = "Edge: localhost:3000",
-      type = "pwa-msedge",
-      request = "launch",
-      url = "http://localhost:3000",
-      webRoot = "${workspaceFolder}/src",
-    },
+-- custom adapter for running tasks before starting debug
+local custom_adapter = "pwa-node-custom"
+dap.adapters[custom_adapter] = function(cb, config)
+        if config.preLaunchTask then
+                local async = require("plenary.async")
+                local notify = require("notify").async
 
-    {
-      name = "Chrome: localhost:3000",
-      type = "pwa-chrome",
-      request = "launch",
-      url = "http://localhost:3000",
-      webRoot = "${workspaceFolder}/src",
-    },
-    {
-      type = "chrome",
-      request = "launch",
-      name = "Next: Chrome",
-      url = "http://localhost:3000",
-      webRoot = "${workspaceFolder}",
-    },
-    {
-      type = "node",
-      request = "launch",
-      name = "Next: Node",
-      outputCapture = "std",
-      program = "./node_modules/next/dist/bin/next",
-      args = "['dev']",
-    },
-  }
+                async.run(function()
+                        ---@diagnostic disable-next-line: missing-parameter
+                        notify("Running [" .. config.preLaunchTask .. "]").events.close()
+                end, function()
+                        vim.fn.system(config.preLaunchTask)
+                        config.type = "pwa-node"
+                        dap.run(config)
+                end)
+        end
+end
+
+-- language config
+for _, language in ipairs({ "typescript", "javascript" }) do
+        dap.configurations[language] = {
+                {
+                        name = "Launch",
+                        type = "pwa-node",
+                        request = "launch",
+                        program = "${file}",
+                        rootPath = "${workspaceFolder}",
+                        cwd = "${workspaceFolder}",
+                        sourceMaps = true,
+                        skipFiles = { "<node_internals>/**" },
+                        protocol = "inspector",
+                        console = "integratedTerminal",
+                },
+                {
+                        name = "Attach to node process",
+                        type = "pwa-node",
+                        request = "attach",
+                        rootPath = "${workspaceFolder}",
+                        processId = require("dap.utils").pick_process,
+                },
+                {
+                        name = "Debug Main Process (Electron)",
+                        type = "pwa-node",
+                        request = "launch",
+                        program = "${workspaceFolder}/node_modules/.bin/electron",
+                        args = {
+                                "${workspaceFolder}/dist/index.js",
+                        },
+                        outFiles = {
+                                "${workspaceFolder}/dist/*.js",
+                        },
+                        resolveSourceMapLocations = {
+                                "${workspaceFolder}/dist/**/*.js",
+                                "${workspaceFolder}/dist/*.js",
+                        },
+                        rootPath = "${workspaceFolder}",
+                        cwd = "${workspaceFolder}",
+                        sourceMaps = true,
+                        skipFiles = { "<node_internals>/**" },
+                        protocol = "inspector",
+                        console = "integratedTerminal",
+                },
+                {
+                        name = "Compile & Debug Main Process (Electron)",
+                        type = custom_adapter,
+                        request = "launch",
+                        preLaunchTask = "npm run build-ts",
+                        program = "${workspaceFolder}/node_modules/.bin/electron",
+                        args = {
+                                "${workspaceFolder}/dist/index.js",
+                        },
+                        outFiles = {
+                                "${workspaceFolder}/dist/*.js",
+                        },
+                        resolveSourceMapLocations = {
+                                "${workspaceFolder}/dist/**/*.js",
+                                "${workspaceFolder}/dist/*.js",
+                        },
+                        rootPath = "${workspaceFolder}",
+                        cwd = "${workspaceFolder}",
+                        sourceMaps = true,
+                        skipFiles = { "<node_internals>/**" },
+                        protocol = "inspector",
+                        console = "integratedTerminal",
+                },
+        }
 end
