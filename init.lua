d371a7e6c861e74645fcc1561a54dc7b91472384
lpp <350331045@qq.com>
2023-03-13 11:07:31 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -61,6 +61,71 @@ local config = {
   },
   -- Configure plugins
   plugins = {
+        {
+      "rebelot/heirline.nvim",
+      opts = function(_, opts)
+        local status = require "astronvim.utils.status"
+
+        opts.winbar = {
+                         -- create custom winbar
+          static = {
+            disabled = {
+                         -- set buffer and file types to disable winbar
+              buftype = { "terminal", "prompt", "nofile", "help", "quickfix" },
+              filetype = { "NvimTree", "neo%-tree", "dashboard", "Outline", "aerial" },
+            },
+          },
+          -- store the current buffer number
+          init = function(self) self.bufnr = vim.api.nvim_get_current_buf() end,
+          fallthrough = false, -- pick the correct winbar based on condition
+          {
+                               -- disabled buffer/file winbar
+            condition = function(self)
+              return vim.opt.diff:get() or status.condition.buffer_matches(self.disabled or {})
+            end,
+            init = function() vim.opt_local.winbar = nil end,
+          },
+          {
+            -- inactive winbar
+            condition = function() return not status.condition.is_active() end,
+            -- show the path to the file relative to the working directory
+            status.component.separated_path { path_func = status.provider.filename { modify = ":.:h" } },
+            -- add the file name and icon
+            status.component.file_info {
+              file_icon = { hl = status.hl.file_icon "winbar", padding = { left = 0 } },
+              file_modified = false,
+              file_read_only = false,
+              hl = status.hl.get_attributes("winbarnc", true),
+              surround = false,
+              update = "BufEnter",
+            },
+          },
+          { -- active winbar
+            -- show the path to the file relative to the working directory
+            status.component.separated_path { path_func = status.provider.filename { modify = ":.:h" } },
+            -- add the file name and icon
+            status.component.file_info { -- add file_info to breadcrumbs
+              file_icon = { hl = status.hl.filetype_color, padding = { left = 0 } },
+              file_modified = false,
+              file_read_only = false,
+              hl = status.hl.get_attributes("winbar", true),
+              surround = false,
+              update = "BufEnter",
+            },
+            -- show the breadcrumbs
+            status.component.breadcrumbs {
+              icon = { hl = true },
+              hl = status.hl.get_attributes("winbar", true),
+              prefix = true,
+              padding = { left = 0 },
+            },
+          },
+        }
+
+        return opts
+      end,
+    },
+
     {
       "dstein64/vim-startuptime",
       -- lazy-load on a command
