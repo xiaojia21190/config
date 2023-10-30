a72083b901fda968354e6ac2d08b56f45589d811
lpp <350331045@qq.com>
2023-10-30 14:25:30 +0800

    Update wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -241,27 +241,26 @@ return {
                 ActivateTabRelative = 1
             }
         },
-
-        -- {
-        --     key = 'LeftArrow',
-        --     mods = 'CTRL',
-        --     action = wezterm.action.ActivatePaneDirection 'Left',
-        --   },
-        --   {
-        --     key = 'RightArrow',
-        --     mods = 'CTRL',
-        --     action = wezterm.action.ActivatePaneDirection 'Right',
-        --   },
-        --   {
-        --     key = 'UpArrow',
-        --     mods = 'CTRL',
-        --     action = wezterm.action.ActivatePaneDirection 'Up',
-        --   },
-        --   {
-        --     key = 'DownArrow',
-        --     mods = 'CTRL',
-        --     action = wezterm.action.ActivatePaneDirection 'Down',
-        --   },
+        {
+            key = 'LeftArrow',
+            mods = 'CTRL|SHIFT',
+            action = wezterm.action.ActivatePaneDirection 'Left',
+        },
+        {
+            key = 'RightArrow',
+            mods = 'CTRL|SHIFT',
+            action = wezterm.action.ActivatePaneDirection 'Right',
+        },
+        {
+            key = 'UpArrow',
+            mods = 'CTRL|SHIFT',
+            action = wezterm.action.ActivatePaneDirection 'Up',
+        },
+        {
+            key = 'DownArrow',
+            mods = 'CTRL|SHIFT',
+            action = wezterm.action.ActivatePaneDirection 'Down',
+        },
         { -- 关闭当前pane
             key = 'w',
             mods = 'CTRL|SHIFT',
